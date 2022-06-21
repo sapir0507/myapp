686ad137ab96c475924f4a467611eb617e191350
sapir shahar <sapir.s@info.cts.org.il>
2022-06-22 00:38:03 +0300

    ברשאן

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import ChildComponent from './ChildComponent';
+import ChildComponent from './project/ChildComponent/ChildComponent';
 
 
 function App() {
   return (
     <div className="App">
       
-        <ChildComponent/> {/* A component should always start with a capital letter or the rendering won't work */}
-        <img src={logo} className="App-logo" alt="logo" />
+        <ChildComponent/> {/* 
+        A component should always start with a capital letter or the rendering won't work.
+
+        this is a second level component in the hierarchy. 
+        */}
+
+        <img src={logo} className="App-logo" alt="logo" /> 
 
     </div>
   );
