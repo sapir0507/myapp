adee2ebe0a5473b9b6c56f192c881b9d1c688717
sapir shahar <sapir.s@info.cts.org.il>
2022-06-22 02:40:07 +0300

    lazy loading

--- src/App.js
+++ src/App.js
@@ -6,12 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-       {/* 
-        A component should always start with a capital letter or the rendering won't work.
-
-        this is a second level component in the hierarchy. 
-        */}
-        <Demo1PropsParent></Demo1PropsParent>
+     
        
 
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const App = lazy(()=>import('./App'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));  //finds element with id="root"
 root.render(
-
-    <App />
+    <Router>
+        <Suspense
+        fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route 
+                    path='/' 
+                    element={<App/>}
+                />
+            </Routes>
+        </Suspense>
+    </Router>
+    // <App />
 
 );
 
