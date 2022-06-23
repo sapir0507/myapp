2736b77703eafe370bca6a47baac5526728a7013
sapir0507 <sapir.s@info.cts.org.il>
2022-06-24 01:06:21 +0300

    ordering the folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 /* eslint-disable react/jsx-pascal-case */
-// import Demo1PropsParent from './project/Demo1_Props/Demo1_PropsParent';
+
 import './App.css';
-import CalculateTotal from './project/Exercises/State-2/calculateTotal';
-// import Demo2_Input from './project/Demo2_Input/Demo2_Input';
-// import PersonComp from './project/PersonComp/PersonComp';
+import CalculateTotal from './project/Exercises/02_State/calculateTotal';
+
 
 
 
