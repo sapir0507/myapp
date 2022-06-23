2028295cdc1e78dc66b4bd6008b034e5226a9923
sapir0507 <sapir.s@info.cts.org.il>
2022-06-23 18:03:02 +0300

    passing obj as props

--- src/project/PersonComp/PersonComp.js
+++ src/project/PersonComp/PersonComp.js
@@ -1,40 +1,79 @@
 import PhoneComp from './PhoneComp/PhoneComp';
-import {Button} from 'react-bootstrap';
+import Button from "react-bootstrap/Button";
+import  Container  from "react-bootstrap/Container";
+import Row  from "react-bootstrap/Row";
+import Col  from "react-bootstrap/Col";
 import {useState } from 'react';
 
-
 function PersonComp() {
-
+    const phones = {
+        'home': '03-221',
+        'mobile': '052-322',
+        'fax': '07-771'
+    }
    const [all, setAll] = useState(false);
-   const [type, setType] = useState(`home phone number`);
+   const [type, setType] = useState({home: phones['home']});
+
 
     return ( <div>
-        <div className='pt-3'></div>
-       <PhoneComp type={type}></PhoneComp>
+       
+        <Container>
+            <Row className="justify-content-md-center mb-4">
+                <Col lg={1} >
+                    <div className='pt-3'></div> 
+                        <Button variant='danger' className='text-white' onClick={()=>{
+                             setAll(!all);
+                        }}>All</Button>
+                </Col>
+           
+            </Row>
+            <hr />
+            {all && <Row>
+                
+                <Container className='border p-3 mt-4' sm='auto'>
+                
+                    {all && Object.keys(phones).map((type, i)=>{
+                        return <PhoneComp key={i} type={type} number={phones[type]}></PhoneComp>
+                    })}
+                
+                   
+                </Container>
+                
+               
+        
+            </Row>}
+        </Container>
        <div className='pt-3'></div>
-        {[
-            'home',
-            'phone',
-            'fax'
-        ].map((phoneType) =>(
-            <span>
-                <Button variant='danger' className='text-white' onClick={()=>{
-                setType(`${phoneType} phone number`)
-            }}>{phoneType}</Button>{' '}
-            </span>
-        ))}
+        <Container>
+            <Row className="justify-content-md-center mb-4">
+                <Col>
+                 {Object.keys(phones).map((phoneType, i) =>(
+                    <span key={`span${i}`}>
+                        <Button variant='danger' key={`btn${i}`} className='text-white' onClick={()=>{
+                            const obj = {
+                                [phoneType]: [phones[phoneType]]
+                            }
+                        setType(obj)
+                    }}>{phoneType}</Button>{' '}
+                    </span>
+                  ))} 
+                </Col>
+            </Row>
+            <Row className='border p-2'>
+                <Col>
+                    <PhoneComp type={Object.keys(type)[0]} number={Object.values(type)[0]}></PhoneComp>
+                </Col>
+            </Row>
+        </Container>
 
-        <Button variant='danger' className='text-white' onClick={()=>{
-            setAll(!all);
-        }}>All</Button>
+       
+     
+        
+       
 
-        <hr />
+        
 
-        {all && <div className="container">
-            <PhoneComp type="Home phone number"></PhoneComp>
-            <PhoneComp type="Phone phone number"></PhoneComp>
-            <PhoneComp type="Fax phone number"></PhoneComp>
-        </div>}
+       
 
     </div>);
 }
--- src/project/PersonComp/PhoneComp/PhoneComp.js
+++ src/project/PersonComp/PhoneComp/PhoneComp.js
@@ -1,10 +1,25 @@
+import Button  from "react-bootstrap/Button";
+import  Container from "react-bootstrap/Container";
+import  Row  from "react-bootstrap/Row";
+import  Col  from "react-bootstrap/Col";
 
 
 function PhoneComp(props) {
 
-    return ( <div>
-        {props.type}
-    </div> );
+    return ( 
+        <Container sm='auto'>
+            <Row className="justify-content-md-center p-1">
+                <Col xs lg="2">
+                    {props.type}: {props.number}
+                </Col>
+                <Col xs lg="2">
+                    <Button variant="info" onClick={()=>{
+                        console.log(props.type, props.number)
+                    }}>Print to console</Button>
+                </Col>
+            </Row>  
+        </Container>
+    );
 }
 
 export default PhoneComp;
